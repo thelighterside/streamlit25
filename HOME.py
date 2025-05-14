e922ee143d8f06ffb652a9d72e0a4ba511729b55
thelighterside <albertpark26@gmail.com>
2025-05-14 14:15:36 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("🌟 Group 3")
 st.caption("This is Group 3's super special secret project! Stay tuned for further updates.")
 
-st.write("This should be a collaboratory work")
+st.write("This will be a collaboratory work...eventually.")
 st.markdown("---")
