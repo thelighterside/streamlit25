0f803e5481ff97d0faaafe17352785bd7c37fbf1
thelighterside <albertpark26@gmail.com>
2025-05-14 14:31:29 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.write("This app is under construction.")
 
-url="https://raw.githubusercontent.com/thelighterside/streamlit25/refs/heads/main/images/Compass.png"
-st.image(url, caption="Image link", width=300)  # width in pixels
+url="https://raw.githubusercontent.com/thelighterside/streamlit25/main/images/Compass.png"
+st.image(url, caption="Image link", width=600)  # width in pixels
 
